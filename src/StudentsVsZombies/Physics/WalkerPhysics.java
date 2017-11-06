3e97dcbba47e79b33d298dd30c3abf67c7a3c29d
Cesar Conterno <conternocesar@gmail.com>
2017-11-06 13:13:43 -0200

    change variable speed to final

--- src/StudentsVsZombies/Physics/WalkerPhysics.java
+++ src/StudentsVsZombies/Physics/WalkerPhysics.java
@@ -5,14 +5,14 @@ import StudentsVsZombies.Spawnable;
 import StudentsVsZombies.State.Walking;
 
 public class WalkerPhysics extends Physics {
-    private double speed = 0.8;
+    final private double speed = 0.8;
     private int update_counter = 0;
     public WalkerPhysics copy() {return new WalkerPhysics();}
     public void update(GameObject o){
         Spawnable obj = (Spawnable) o;
         if(obj.state instanceof Walking) {
             update_counter++;
-            if (update_counter > (int) (1 / speed)){
+            if (update_counter > 1 / speed){
                 update_counter = 0;
                 obj.x_ -= 1;
             }
